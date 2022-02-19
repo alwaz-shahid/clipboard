5673b048d16431d91e44d0815bec589bc93461aa
Alwaz18 <alwazk8@gmail.com>
2022-02-19 18:18:35 +0500

    nf

--- numpy/exercises.py
+++ numpy/exercises.py
@@ -80,17 +80,23 @@ def ex6():
     print(o)
 
 
-def ex7():
+def ex7(stack_dir="h"):
     """Stack 2 numpy arrays horizontally i.e., 2 arrays having the same 1st dimension (number of rows in 2D arrays)"""
     a1 = np.array([[1, 2, 3],
                    [4, 5, 6]])
 
     a2 = np.array([[7, 8, 9],
                    [10, 11, 12]])
-
-    o = np.hstack((a1, a2))
-
-    print(o)
+    # use vstack to stack c=vertically
+    if stack_dir == "h":
+        # c = np.hstack((a1, a2))
+        o = np.hstack((a1, a2))
+        print(o)
+    elif stack_dir == "v":
+        o = np.vstack((a1, a2))
+        print(o)
+    else:
+        print("Invalid direction, v/h")
 
 
 switcher = {1: ex1, 2: ex2, 3: ex3, 4: ex4, 5: ex5, 6: ex6, 7: ex7}
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import numpy as np
 edit = False
 
 
@@ -26,3 +27,41 @@ while (edit):
 
 
 # makeKwargs(a=1, b=2, c=3)
+
+
+# Implement Python Switch Case Statement using Class
+
+class PythonSwitch:
+
+    def switch(self, dayOfWeek):
+        default = "Incorrect day"
+        return getattr(self, 'case_' + str(dayOfWeek), lambda: default)()
+
+    def case_1(self):
+        return "Monday"
+
+    def case_2(self):
+        return "Tuesday"
+
+    def case_3(self):
+        return "Wednesday"
+
+
+s = PythonSwitch()
+
+print(s.switch(1))
+print(s.switch(3))
+# print(s.switch(0))
+
+
+def alternate_sols(sol=1):
+    if sol == 1:
+        list_of_numbers = [x for x in range(0, 101, 2)]
+
+        o = np.array(list_of_numbers)
+
+        print(o)
+    elif sol == 2:
+        o = np.arange(0, 101, 2)
+
+        print(o)
