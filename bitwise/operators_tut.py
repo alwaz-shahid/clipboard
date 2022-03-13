e5addb194a52d41b0e71a2a14544f23ca961d337
Alwaz18 <alwazk8@gmail.com>
2022-03-13 12:05:23 +0500

    game

---
+++ bitwise/operators_tut.py
@@ -0,0 +1,34 @@
+# in binary 0010 0101..add()
+# num2 = 0b100101  # binary
+
+
+"""If two ints 5-7
+both will be converted to binary
+and stacked vertically
+if both have 1,1 the output will be 1
+example:
+for the sake of example we will 8 bit binary numbers.
+37 = 00100101
+23 = 00000101
+op = nnnnnyny
+we have : 00000101
+binary will be 5 
+as well as the result of the operation
+now result^^^^^
+
+random line
+op=0b100101 + 0b00000101 = 0b10010101?
+"""
+
+
+def convert_to_binary(*args):
+    """convert to binary"""
+    # return bin(num)[2:]
+    # res = bin(num)[2:]
+    # res = bin(num)[2:].zfill(8)
+    # res =
+    # print(res)
+    return None
+
+
+convert_to_binary(5)
--- import_mods/app.py
+++ import_mods/app.py
@@ -1,6 +1,29 @@
 import test_f
 from nestedF.new_F import nested_def
-test_f.my_function("--Hello", "World--", "--Python--",
-                   f_name="alaz", l_name="a22laz")
+# test_f.my_function("--Hello", "World--", "--Python--",
+#    f_name="alaz", l_name="a22laz")
 
-nested_def("--new", "sss--", "--thon--")
+# nested_def("--new", "sss--", "--thon--")
+
+# function print hyer name
+
+# car = {
+#     'brand': 'Ford',
+#     'model': 'Mustang',
+#     'year': 1964
+# }
+# x = car.setdefault('color', 'white')
+# print(x)
+
+
+def yell():
+    print("YELL uo")
+
+
+what_val = 2
+
+a = 2
+b = 2
+
+c = a == b & a != ~what_val & yell()
+print(c)
